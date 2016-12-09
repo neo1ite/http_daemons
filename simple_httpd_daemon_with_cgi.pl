2ed707370060f232d5d7c4ed6148c0ca79b0afac
Neo1ite <neo1515@gmail.com>
2016-12-09 16:12:38 +0300

    Add cgi-script and simple http server with execution cgi scripts support

---
+++ simple_httpd_daemon_with_cgi.pl
@@ -0,0 +1,79 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Data::Dumper;
+
+use LWP 5.23;
+use HTTP::Daemon;
+use File::Basename;
+use Sys::Hostname;
+
+use constant DEBUG          => 0;
+use constant DOCUMENT_ROOT  => './www';
+use constant SCRIPT_PATH    => './www/cgi-bin';
+use constant USE_ERROR_LOG  => 1;
+use constant ERROR_LOG_FILE => (fileparse($0, qr/\.[^.]*/))[0] . '.error.log';
+use constant CRLF           => "\015\012";
+
+print "Starting server...\n";
+my $server = HTTP::Daemon->new(LocalAddr => 'localhost', Timout => 60, LocalPort => 80);
+
+print "Server started successfully. Wating for connection...\n";
+while (my $client = $server->accept) {
+   while (my $request = $client->get_request) {
+      if ($request->method eq 'GET') {
+         my $path = $request->url->path;
+         my $server_name = $request->header('Host') || lc Sys::Hostname::hostname() || inet_ntoa($client->sockaddr);
+
+         if (-d DOCUMENT_ROOT . $path && $path !~ /\/$/)
+         {
+            my $url = 'http://' . $server_name . $path . '/';
+            print "Redirecting to $url\n" if DEBUG;
+            $client->send_redirect($url)
+         }
+
+         $path = (-d DOCUMENT_ROOT . $path && -f DOCUMENT_ROOT . $path . 'index.html')
+            ? DOCUMENT_ROOT . $path . 'index.html'
+            : DOCUMENT_ROOT . $path;
+
+         if (-d $path) { $client->send_error(403); }
+         if (-f $path) {
+            if ($path =~ /\.pl$/)
+            {
+               my $pipe_line = $^X . ' ' . $path . ' 2>' . (USE_ERROR_LOG ? ERROR_LOG_FILE : ($^O =~ /MSWin32/ ? 'NUL' : '/dev/null')) . '|';
+               print "Opening pipe $pipe_line\n" if DEBUG;
+
+               my $pipe;
+               open($pipe, $pipe_line);
+               my $header = <$pipe>;
+               my $lines;
+               {
+                  local $/ = '';
+                  $lines = <$pipe>;
+               }
+               close($pipe);
+
+               $client->send_error(500) unless (length($header) <= 30);
+               $client->send_basic_header();
+
+               print $client $header;
+               print $client 'Content-Length: ' . length($lines) . CRLF;
+               print $client CRLF;
+               print $client $lines;
+            }
+            else
+            {
+               print "Sending file $path\n" if DEBUG;
+               $client->send_file_response($path);
+            }
+         } else {
+            $client->send_error(404);
+         }
+      }
+   }
+   $client->close;
+   undef($client);
+}
+
+exit(0);
---
+++ www/cgi-bin/env.pl
@@ -0,0 +1,14 @@
+﻿#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Data::Dumper;
+use utf8;
+
+print "Content-type: text/plain\n\n";
+
+$Data::Dumper::Sortkeys = 1;
+
+print Data::Dumper::Dumper \%ENV;
+
+exit(0);
